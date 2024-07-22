d7be78b36057697d30ba13a4ccacd4f116f435ec
Gernot Schmitz <spammail1240@web.de>
2024-07-22 11:48:07 +0200

    try shifting print statements

--- test_model/test_model.py
+++ test_model/test_model.py
@@ -157,6 +157,8 @@ class TestShap(unittest.TestCase):
                 print(f'Computing contribution and hypothetical contribution scores for {plant}-----------------------------\n')
                 res = self.compute_actual_and_hypothetical_scores(fasta_file, gtf_file, counts, plant)
                 print("resulting explanations: ", res)
+                res = self.compute_actual_and_hypothetical_scores(fasta_file, gtf_file, counts, plant)
+                print("resulting explanations: ", res)
                 print(f'Running TFMoDisco on {plant}------------------------------------------------------------------------\n')
     
     # def test_deep_shap(self):
