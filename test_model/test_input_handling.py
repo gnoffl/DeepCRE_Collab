cbe2f17b6e65555c25237e8b91604baba8b7e146
Gernot Schmitz <spammail1240@web.de>
2024-08-02 17:39:09 +0200

    fixed imports. added method to create mock data for testing.

--- test_model/test_input_handling.py
+++ test_model/test_input_handling.py
@@ -6,10 +6,14 @@ import numpy as np
 import pandas as pd
 import pickle
 import math
+import os
+import sys
 
 from typing import Tuple, List
-
-import extract_sequences
+model_path = os.path.join(os.path.dirname(__file__), "..", "model")
+sys.path.insert(1, model_path)
+from utils import prepare_valid_seqs
+import input_handling as ih
 
 # test empty sequence
 # test names being found in output
@@ -28,7 +32,30 @@ def test_fasta():
     print(fasta_obj["1"])
 
 
+def write_mockup_data():
+    sequence = "A" * 500 + "C" * 500 + "T" * 500 + "G" * 500
+    sequence = sequence * 5
+    name = "1 mock_up_sequence"
+    fasta_path = os.path.join(os.path.dirname(__file__), "test_data", "test_input.fa")
+    with open(fasta_path, "w") as f:
+        f.write(f">{name}\n")
+        for i in range(0, len(sequence) + 1, 80):
+            f.write(f"{sequence[i:i+80]}\n")
+        if i != len(sequence):
+            f.write(f"{sequence[i+80:-1]}\n")
+    
+
+
 if __name__ == "__main__":
-    extract_sequences.extract_gene_flanking_regions(fasta_path="/home/gernot/Downloads/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa", gff_path="/home/gernot/Downloads/Arabidopsis_thaliana.TAIR10.59.gff3", output_path="test.fa", extract_one_hot_flag=False, extract_string_flag=True, extragenic=100, intragenic=50)
+    #todo: 
+    #   create simple test sequence + annotation
+    #   make sure this works with string as well as one hot extraction
+    #   add argument parser to extract stuff from anywhere, if necessary??
+    # ih.extract_gene_flanking_regions(fasta_path="/home/gernot/Downloads/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa",
+    #                                  gff_path="/home/gernot/Downloads/Arabidopsis_thaliana.TAIR10.59.gff3",
+    #                                  output_path="test.fa", extract_one_hot_flag=False, extract_string_flag=True, 
+    #                                  extragenic=100, intragenic=50)
+    write_mockup_data()
+
 
     # test_fasta()
